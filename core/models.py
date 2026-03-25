f34390a74d9a7ee54e05f298eba8c39e4616d55d
Kacper <kogutkacper7@gmail.com>
2026-03-25 16:17:23 +0100

    Add base models: Architecture, custom Researcher, TrainModel, PerfomanceMetric & register models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Researcher(AbstractUser):
+    specialization = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username
+
+
+class Architecture(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Name")
+    description = models.TextField(max_length=255, verbose_name="Description")
+    config = models.JSONField(blank=True, null=True, verbose_name="Konfiguracja JSON")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
+
+    def __str__(self):
+        return self.name
+
+class TrainModel(models.Model):
+    name = models.CharField(max_length=255)
+    version = models.FloatField()
+    architecture = models.ForeignKey(Architecture, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class PerformanceMetric(models.Model):
+    trained_model = models.ForeignKey(TrainModel, on_delete=models.CASCADE)
+    accuracy_score = models.FloatField()
+    loss_value = models.FloatField()
+    test_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Name {self.trained_model}. Accuracy: {self.accuracy_score}."
