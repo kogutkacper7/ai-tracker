b6ad4672317896aa726a0e23aa5d317877c2d635
Kacper <kogutkacper7@gmail.com>
2026-04-03 15:30:10 +0200

    add login and logout function

--- ai_tracker/settings.py
+++ ai_tracker/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
-from django.conf.global_settings import AUTH_USER_MODEL
+from django.conf.global_settings import AUTH_USER_MODEL, LOGIN_REDIRECT_URL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,3 +121,6 @@ STATIC_URL = 'static/'
 
 
 AUTH_USER_MODEL = 'core.Researcher'
+
+LOGOUT_REDIRECT_URL = "core:login"
+LOGIN_REDIRECT_URL = "core:index"
--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import ResearcherListView, index_view, TrainModelListView, TrainModelDetailView, ResearcherDetailView, ArchitectureDetailView
-
+from django.conf.urls import include
 app_name = "core"
 
 urlpatterns = [
     path("", index_view, name="index"),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("researcher-list/", ResearcherListView.as_view(), name="researcher-list"),
     path("train_model-list/", TrainModelListView.as_view(), name="train_model-list"),
     path("train_model-detail/<int:pk>/", TrainModelDetailView.as_view(), name="train-model-detail"),
