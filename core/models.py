f045a0a7b0e233da98d20afe7c3a56bc99937794
Kacper <kogutkacper7@gmail.com>
2026-04-14 19:43:47 +0200

    Add tests for models

--- core/models.py
+++ core/models.py
@@ -27,14 +27,14 @@ class Architecture(models.Model):
         return self.name
 
 class TrainModel(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     version = models.CharField(max_length=255)
     architecture = models.ForeignKey(Architecture, on_delete=models.CASCADE)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="train_models")
     tags = models.ManyToManyField(Tag)
 
     def __str__(self):
-        return self.name
+        return f"Train model name: {self.name}"
 
     def get_absolute_url(self):
         return reverse("core:train-model-detail", kwargs={"pk": self.pk})
@@ -46,7 +46,7 @@ class PerformanceMetric(models.Model):
     test_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Name {self.trained_model}. Accuracy: {self.accuracy_score}."
+        return f"Name {self.trained_model.name}. Accuracy: {self.accuracy_score}."
 
     def get_absolute_url(self):
         return reverse("core:train-model-detail", kwargs={"pk":self.trained_model.pk})
